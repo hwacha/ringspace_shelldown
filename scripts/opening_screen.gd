c2bbde26f3642e20da25a562f2fff70f9ce2f850
Bill Marcy <wgmarcy@gmail.com>
2023-05-02 03:10:18 -0400

    add controller support

--- scripts/opening_screen.gd
+++ scripts/opening_screen.gd
@@ -21,29 +21,6 @@ func get_input():
 			if Input.is_action_just_pressed(action + "_p" + str(i)):
 				set_registered_players(i)
 				break
-	
-	var master_sound = AudioServer.get_bus_index("Master")
-	var is_muted = AudioServer.is_bus_mute(master_sound)
-	if Input.is_action_just_pressed("settings_toggle_mute"):
-		AudioServer.set_bus_mute(master_sound, not is_muted)
-	
-	if Input.is_action_just_pressed("settings_increment_orb_loss_numerator"):
-		if Players.orb_loss_numerator < Players.orb_loss_denominator:
-			Players.orb_loss_numerator += 1
-	if Input.is_action_just_pressed("settings_decrement_orb_loss_numerator"):
-		if Players.orb_loss_numerator > 0:
-			Players.orb_loss_numerator -= 1
-	if Input.is_action_just_pressed("settings_increment_orb_loss_denominator"):
-		Players.orb_loss_denominator += 1
-	if Input.is_action_just_pressed("settings_decrement_orb_loss_denominator"):
-		if Players.orb_loss_denominator > 1 and \
-		Players.orb_loss_numerator < Players.orb_loss_denominator:
-			Players.orb_loss_denominator -= 1
-		
-	$Settings.text = "[1] mute: " + str(is_muted) + \
-					"\n[2, 3, 4, 5] orb_loss_fraction: " + \
-					str(Players.orb_loss_numerator) + \
-					"/" + str(Players.orb_loss_denominator)
 
 	if Input.is_action_just_pressed("ui_accept"):
 		Players.set_player_ids(registered_players)
