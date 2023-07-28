1b04d76b111d3b0704cd678d1ab514a0d28eb913
Bill Marcy <wgmarcy@gmail.com>
2023-07-28 01:04:54 -0400

    fix teleport animation
    all powerups

--- scripts/TeleportAnimation.gd
+++ scripts/TeleportAnimation.gd
@@ -7,7 +7,13 @@ var destination : Vector2
 
 var animation_player : AnimationPlayer
 
+var powerup
+
 func _ready():
+	powerup = player.get_node("TrailingPowerup")
+	player.remove_child(powerup)
+	add_child(powerup)
+	player.get_node("ShadowSprite").visible = false
 	color = player.color + Color(0.2, 0.2, 0.2, -0.3)
 	var animation = Animation.new()
 	animation.length = 0.5
@@ -33,15 +39,24 @@ func _ready():
 	add_child(animation_player)
 
 func _process(_delta):
+	if powerup != null:
+		var corner_powerup = get_node("/root/Main/Powerups/Powerup" + str(player.id))
+		var corner_powerup_sprite = corner_powerup.get_node("Sprite2D")
+		powerup.texture = corner_powerup_sprite.texture
+		powerup.modulate = corner_powerup.modulate * corner_powerup_sprite.modulate
+			
 	queue_redraw()
 	
 func _draw():
 	draw_line(source, destination, color, 8)
 
 func _on_animation_finished(_anim):
+	remove_child(powerup)
+	player.add_child(powerup)
 	player.transform.origin = destination
 	player.visible = true
 	player.lock_physics = false
+	
 	
 	get_parent().remove_child(self)
 	queue_free()
