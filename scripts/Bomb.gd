5d3c83305bc0e92330b406344bd83c7fb7a5e0e9
Bill Marcy <wgmarcy@gmail.com>
2023-07-30 00:50:30 -0400

    fix bomb sd bug

--- scripts/Bomb.gd
+++ scripts/Bomb.gd
@@ -60,8 +60,9 @@ func _process(delta):
 			if not player.dead:
 				if status == BombStatus.BLASTING:
 					if self.position.distance_to(player.position) < 80:
-						player.killer = player_who_dropped
-						player.killer_id = player_who_dropped_id
+						if player != player_who_dropped:
+							player.killer = player_who_dropped
+							player.killer_id = player_who_dropped_id
 						player.die()
 					else:
 						player.stunned = true
