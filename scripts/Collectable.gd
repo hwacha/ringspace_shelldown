499d4f091f011d40c8ec84a66e9da009efe275dd
Bill Marcy <wgmarcy@gmail.com>
2023-08-01 15:49:03 -0400

    make it so powerups randomly assigned on ties

--- scripts/Collectable.gd
+++ scripts/Collectable.gd
@@ -1,21 +1,22 @@
 extends Area2D
 
-var claimed = false
 var collectable = null : set = set_collectable
+
+var claimants = []
 
 func _ready():
 	$AnimationPlayer.play("spawn")
 
 func _process(_delta):
-	pass
+	if claimants.size() > 0:
+		var claimant_id = claimants.pick_random()
+		get_node("../Powerups/Powerup" + str(claimant_id)).set_powerup(collectable)
+		get_parent().call_deferred("remove_child", self)
+		queue_free()
 
 func set_collectable(new_collectable):
 	collectable = new_collectable
 	$Sprite2D.texture = load("res://assets/" + new_collectable + ".png")
 
 func _on_body_entered(body):
-	if not claimed:
-		get_node("../Powerups/Powerup" + str(body.id)).set_powerup(collectable)
-		get_parent().call_deferred("remove_child", self)
-		queue_free()
-		claimed = true
+	claimants.push_back(body.id)
